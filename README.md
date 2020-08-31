6d14c49a600729e2c0e72b89bd5175af4a50d8de
Scott Doxey <neogeek0101@gmail.com>
2020-08-31 00:01:11 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+# @neogeek/videoplayer.js
+
+> 🎥 Display video in the browser with variable input sources.
+
+## Install
+
+```bash
+$ npm install @neogeek/videoplayer.js
+```
---
+++ index.js
@@ -0,0 +1,78 @@
+const createDeviceOption = (device, select) => {
+    const option = document.createElement('option');
+
+    option.setAttribute('value', device.deviceId);
+
+    option.appendChild(document.createTextNode(device.label));
+
+    select.appendChild(option);
+};
+
+const setDeviceId = (key, value) => {
+    localStorage.setItem(`${key}DeviceId`, value);
+};
+
+const getDeviceId = key => {
+    return localStorage.getItem(`${key}DeviceId`);
+};
+
+const setVideoPlayerObject = video => {
+    const videoInputDeviceId = getDeviceId('videoInput');
+    const audioInputDeviceId = getDeviceId('audioInput');
+
+    navigator.mediaDevices
+        .getUserMedia({
+            video: videoInputDeviceId ? { deviceId: videoInputDeviceId } : true,
+            audio: audioInputDeviceId ? { deviceId: audioInputDeviceId } : true
+        })
+        .then(stream => {
+            video.srcObject = stream;
+        });
+};
+
+const setupVideoPlayer = (video, options = {}) => {
+    setVideoPlayerObject(video);
+
+    const devices = navigator.mediaDevices.enumerateDevices();
+
+    if (options.videoInputDeviceSelect) {
+        devices
+            .then(devices =>
+                devices.filter(({ kind }) => kind === 'videoinput')
+            )
+            .then(devices =>
+                devices.map(device =>
+                    createDeviceOption(device, options.videoInputDeviceSelect)
+                )
+            );
+        options.videoInputDeviceSelect.addEventListener(
+            'change',
+            ({ target: { value } }) => {
+                setDeviceId('videoInput', value);
+
+                setVideoPlayerObject(video);
+            }
+        );
+    }
+
+    if (options.audioInputDeviceSelect) {
+        devices
+            .then(devices =>
+                devices.filter(({ kind }) => kind === 'audioinput')
+            )
+            .then(devices =>
+                devices.map(device =>
+                    createDeviceOption(device, options.audioInputDeviceSelect)
+                )
+            );
+        options.videoInputDeviceSelect.addEventListener(
+            'change',
+            ({ target: { value } }) => {
+                setDeviceId('audioInput', value);
+
+                setVideoPlayerObject(video);
+            }
+        );
+    }
+};
+export { setVideoPlayerObject, setupVideoPlayer };
